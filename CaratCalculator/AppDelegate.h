181c718fef895f612d13951bb07bcf5c36fdf096
Robert Koczon <robert.koczon@gmail.com>
2013-01-19 12:42:27 -0800

    Initial Commit

---
+++ CaratCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CaratCalculator
+//
+//  Created by Bobby Koczon on 1/19/13.
+//  Copyright (c) 2013 Bobby Koczon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
